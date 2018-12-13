a86290e1914ca41ac3e68a85da9730e18e391368
prasad17y <prasad17y@gmail.com>
2018-12-13 05:32:20 +0530

    test2

--- sample_code.c
+++ sample_code.c
@@ -2,7 +2,6 @@
 int main()
 {
 	printf("this is sample c code");
-	printf("some change in this file");
-	printf("some change done through github.com");
+	printf("some change in this file 2");
 	return 0;
 }
